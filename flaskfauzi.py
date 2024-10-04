bb00b6ea07269ce96a3397d1e5e1dd79382637b8
fauzi1999mei <riskysingaraja2018@gmail.com>
2024-10-04 23:13:48 +0700

    second commit

--- flaskfauzi.py
+++ flaskfauzi.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    #html file
-    return "hello world, and congratulations fauzi"
+# @app.route('/')
+# def hello():
+#     #html file
+#     return "hello world, and congratulations fauzi"
 
-@app.route('/about')
-def about():
+@app.route('/index')
+def index():
     # html file
-    return "about me"
+    return render_template('index.html')
 
 if __name__== '__main__':
     app.run()
